241a0d8d55613b1f32a8a297c1deb00564bfbc59
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-14 10:02:56 +0300

    feat(authentication): add initial auth REST API

---
+++ app/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Database\UniqueConstraintViolationException;
+
+class AuthController extends BaseController
+{
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        try {
+        $user = User::create($input);
+        $success['user'] =  $user;
+        } catch (UniqueConstraintViolationException $e) {
+            return $this->sendError('Duplicate entry.', ['error' => 'User already exists.']);
+        }
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
+
+        $success = $this->respondWithToken($token);
+
+        return $this->sendResponse($success, 'User login successfully.');
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function profile()
+    {
+        $success = Auth::user();
+
+        return $this->sendResponse($success, 'Refresh token return successfully.');
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        Auth::logout();
+
+        return $this->sendResponse([], 'Successfully logged out.');
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        $success = $this->respondWithToken(Auth::refresh());
+
+        return $this->sendResponse($success, 'Refresh token return successfully.');
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ];
+    }
+}
