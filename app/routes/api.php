011492c545e239dfd42ba23e38f9020522498922
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-15 05:26:27 +0300

    fix(TodoController): rename namespace

--- app/routes/api.php
+++ app/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\API\TodoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
